a0f270109d18f63ffcfc3d067222cb541deb4fc7
Alexandru Burghelea <burghelea.alex@gmail.com>
2017-01-26 20:10:08 +0200

    Update 5

--- app/src/main/java/adapters/ImageAdapter.java
+++ app/src/main/java/adapters/ImageAdapter.java
@@ -49,9 +49,8 @@ public class ImageAdapter extends BaseAdapter {
         if (convertView == null) {
             // if it's not recycled, initialize some attributes
             imageView = new ImageView(mContext);
-            imageView.setLayoutParams(new GridView.LayoutParams(85, 85));
-            imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
-            imageView.setPadding(8, 8, 8, 8);
+            imageView.setLayoutParams(new GridView.LayoutParams(200, 200));
+            imageView.setScaleType(ImageView.ScaleType.CENTER_INSIDE);
         } else {
             imageView = (ImageView) convertView;
         }
